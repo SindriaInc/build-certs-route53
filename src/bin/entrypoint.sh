43f79b9fb9389669ad54bff86a44d2b15165e08e
lucapitzoi <luca.pitzoi@sindria.org>
2021-08-27 18:23:26 +0200

    Added base logic

--- src/bin/entrypoint.sh
+++ src/bin/entrypoint.sh
@@ -12,44 +12,28 @@ NOW=$(date "+%Y-%m-%d_%H-%M-%S")
 
 if [ "${IAC_MODE}" == "standalone" ]; then
 
-  # Validation lightsail name
-  if [ "${LIGHTSAIL_NAME}" == "" ]; then
-      echo -e "${YELLOW}LIGHTSAIL_NAME env must be provided${NC}"
-      exit 1
-  fi
 
-  # Validation lightsail port
-  if [ "${LIGHTSAIL_PORT}" == "" ]; then
-      echo -e "${YELLOW}LIGHTSAIL_PORT env must be provided${NC}"
-      exit 1
-  fi
+   mkdir -p /etc/letsencrypt
+   # Renew existing cert or build
+   certbot renew -n --agree-tos --dns-route53 --dns-route53-propagation-seconds 60 --cert-name ${IAC_CERTBOT_DOMAIN} -m ${IAC_CERTBOT_EMAIL} || certbot certonly -n --agree-tos --dns-route53 --dns-route53-propagation-seconds 60 -d ${IAC_CERTBOT_DOMAIN} -d *.${IAC_CERTBOT_DOMAIN} -m ${IAC_CERTBOT_EMAIL}
 
-  # Validation lightsail protocol
-  if [ "${LIGHTSAIL_PROTOCOL}" == "" ]; then
-    LIGHTSAIL_PROTOCOL="TCP"
-  fi
-
-  # Validation lightsail cidr
-  if [ "${LIGHTSAIL_CIDR}" == "" ]; then
-    LIGHTSAIL_CIDR="0.0.0.0/0"
-  fi
-
-
-  # Setting lightsail values - if security.yaml is overwritten by volume this simply doesn't take any effect. (it's a feature not a bug)
-  sed -i -E "s|@@NAME@@|${LIGHTSAIL_NAME}|g" /var/www/app/config/security.yaml
-  sed -i -E "s|@@PORT@@|${LIGHTSAIL_PORT}|g" /var/www/app/config/security.yaml
-  sed -i -E "s|@@PROTOCOL@@|${LIGHTSAIL_PROTOCOL}|g" /var/www/app/config/security.yaml
-  sed -i -E "s|@@CIDR@@|${LIGHTSAIL_CIDR}|g" /var/www/app/config/security.yaml
 else
-  # Cleanup config directory
-  rm -Rf /var/www/app/config
-  mkdir -p /var/www/app/config
-  # Infra setup
-  mkdir -p .setup
-  cd .setup
-  git clone https://${IAC_GIT_USERNAME}:${IAC_GIT_PASSWORD}@${IAC_GIT_PROVIDER}/${IAC_GIT_NAMESPACE}/${IAC_INFRA_NAME}.git
-  cp ${IAC_INFRA_NAME}/config/* ../config
+
+  # Init certbot cache
+  mkdir -p /etc/letsencrypt
+  aws s3 sync s3://${IAC_CERTBOT_CACHE} /etc/letsencrypt
+
+  # Cleanup symblinks
+  rm -f /etc/letsencrypt/live/${IAC_CERTBOT_DOMAIN}/*.pem || true
+  ln -s /etc/letsencrypt/archive/${IAC_CERTBOT_DOMAIN}/cert*.pem /etc/letsencrypt/live/${IAC_CERTBOT_DOMAIN}/cert.pem || true
+  ln -s /etc/letsencrypt/archive/${IAC_CERTBOT_DOMAIN}/chain*.pem /etc/letsencrypt/live/${IAC_CERTBOT_DOMAIN}/chain.pem || true
+  ln -s /etc/letsencrypt/archive/${IAC_CERTBOT_DOMAIN}/fullchain*.pem /etc/letsencrypt/live/${IAC_CERTBOT_DOMAIN}/fullchain.pem || true
+  ln -s /etc/letsencrypt/archive/${IAC_CERTBOT_DOMAIN}/privkey*.pem /etc/letsencrypt/live/${IAC_CERTBOT_DOMAIN}/privkey.pem || true
+
+  # Renew existing cert or build
+  certbot renew -n --agree-tos --dns-route53 --dns-route53-propagation-seconds 60 --cert-name ${IAC_CERTBOT_DOMAIN} -m ${IAC_CERTBOT_EMAIL} || certbot certonly -n --agree-tos --dns-route53 --dns-route53-propagation-seconds 60 -d ${IAC_CERTBOT_DOMAIN} -d *.${IAC_CERTBOT_DOMAIN} -m ${IAC_CERTBOT_EMAIL}
+
+  # Update certbot cache
+  aws s3 sync /etc/letsencrypt s3://${IAC_CERTBOT_CACHE}
 fi
 
-# Run lightsail instance public ports
-python /var/www/app/app/main.py
